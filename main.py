c50c5328823591b636f8a4bcdee38fe48fae6a7e
Lukas da Rosa <lukas-sr@hotmail.com>
2021-07-18 18:46:52 -0300

    modify main.py to fix bug

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def StockNameImportation():
 
 if __name__ == "__main__":
     stock_names, stock_files = StockNameImportation()
-
-    # create a dictionary with the ticker information removed from the statements folder 
+    
+    # create a dictionary with the ticker information
+    # 
     # fund = {
     #       "ABEV3": statement_ABEV3,
     #       "MGLU3": statement_MGLU3
@@ -24,30 +25,37 @@ if __name__ == "__main__":
     #       }
 
     fund = {}
-     
+
     for file_name in stock_files:
-        name = file_name[-9:-4]
+
+        name = file_name[3:8]
+        
         if "11" in name:
-            name = file_name[-10:-4]
+            name = file_name[3:9]
 
-        if name in stock_names:
-            balanco = pd.read_excel(f'statements/{file_name}', sheet_name=0)
+        balanco = pd.read_excel(f'statements/{file_name}', sheet_name=0)
         
-            # turn first line into a header
-            balanco.columns = balanco.iloc[0]
-            balanco = balanco[1:]
+        # first column place the title as the name of the company
+        balanco.iloc[0,0] = name 
 
-            # first column place the title as the name of the company
-            balanco = balanco.set_index(name) 
+        # turn first line into a header and exclude the duplicated
+        balanco.columns = balanco.iloc[0]
+        balanco = balanco[1:]
 
-            dre = pd.read_excel(f'statements/{file_name}', sheet_name=1)
+        balanco = balanco.set_index(name)
+     
+        # do the same to the 'dre's
+        dre = pd.read_excel(f'statements/{file_name}', sheet_name=1)
+    
+        dre.iloc[0,0] = name
+
+        dre.columns = dre.iloc[0]
+        dre = dre[1:]
+
+        dre = dre.set_index(name)
+
+        fund[name] = balanco.append(dre)
         
-            dre.columns = dre.iloc[0]
-            dre = dre[1:]
-
-            dre = dre.set_index(name)
-
-            fund[name] = balanco.append(dre)
-            print(fund)
-            break
+        print(fund)
+        break
 
