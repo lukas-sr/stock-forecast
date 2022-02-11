8b38e28ce59fc1b89c43f0868601aa934418d71c
lukas-sr <lukas-sr@hotmail.com>
2022-02-11 08:34:31 -0300

    update main.py

--- main.py
+++ main.py
@@ -3,31 +3,34 @@ import pandas as pd
 
 def StockNameImportation():
     stock_list = listdir('statements')
-    new = []
+    ticker = []
     
     for name in stock_list:
         if (name[8] == '1'):
-            new.append(name[3:9])
+            ticker.append(name[3:9])
         else: 
-            new.append(name[3:8])
+            ticker.append(name[3:8])
 
-    return new, stock_list
+    return ticker, stock_list
 
 if __name__ == "__main__":
     stock_names, stock_files = StockNameImportation()
     
+    #
     # stock_names = ["ABEV3", "AZUL4", "BTOW3", "B3SA3", "BBSE3", "BRML3", "BBDC4", "BRAP4", "BBAS3", "BRKM5", "BRFS3", "BPAC11",
     # "CRFB3", "CCRO3", "CMIG4", "HGTX3", "CIEL3", "COGN3", "CPLE6", "CSAN3", "CPFE3", "CVCB3", "CYRE3", "ECOR3", "ELET6", "EMBR3",
     # "ENBR3", "ENGI11", "ENEV3", "EGIE3", "EQTL3", "EZTC3", "FLRY3", "GGBR4", "GOAU4", "GOLL4", "NTCO3", "HAPV3", "HYPE3", "IGTA3", 
     # "GNDI3", "ITSA4", "ITUB4", "JBSS3", "JHSF3", "KLBN11", "RENT3", "LCAM3", "LAME4", "LREN3", "MGLU3", "MRFG3", "BEEF3", "MRVE3",
     # "MULT3", "PCAR3", "PETR4", "BRDT3", "PRIO3", "QUAL3", "RADL3", "RAIL3", "SBSP3", "SANB11", "CSNA3", "SULA11", "SUZB3",
     # "TAEE11", "VIVT3", "TIMS3", "TOTS3", "UGPA3", "USIM5", "VALE3", "VVAR3", "WEGE3", "YDUQ3"] 
+    #
+
     # 
-    # create a dictionary with the ticker information
+    # Next step: create a dictionary with the ticker information
     # 
     # fund = {
     #       "ABEV3": statement_ABEV3,
-    #       "MGLU3": statement_MGLU3
+    #       "MGLU3": statement_MGLU3,
     #       ...
     #       }
 
@@ -92,6 +95,8 @@ if __name__ == "__main__":
         if (quotation[company].isnull().values.any()):
             quotation.pop(company)
             fund.pop(company)
+    
+    company = list(quotation.keys())
 
     # Update stock names list
     stock_names = list(quotation.keys())
